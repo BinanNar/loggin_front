6b2f24091c6c60dcb5ec846a7279e0dca880c399
BinanNar <halflifemaster1234@gmail.com>
2023-02-13 17:34:45 -0700

    diseño inicial

---
+++ src/views/logeado.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-row justify="center">
+        <h1>estas logeado {{ nombre }}</h1>
+    </v-row>
+</template>
+
+<script>
+
+
+  export default {
+
+    data: () => ({
+    //
+    nombre:"......",
+
+  }),
+    
+    methods:{
+
+    }
+    ,
+    mounted(){
+        console.log(this.$route)
+        this.nombre = this.$route.query.nombre
+        
+    }
+  }
+</script>

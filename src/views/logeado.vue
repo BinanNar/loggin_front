4d396484eb8927aed8ddfa36ed77ac7ff32dc437
BinanNar <halflifemaster1234@gmail.com>
2023-02-22 17:09:48 -0700

    agregado logout

--- src/views/logeado.vue
+++ src/views/logeado.vue
@@ -1,7 +1,12 @@
 <template>
+<v-container>
     <v-row justify="center">
         <h1>estas logeado {{ nombre }}</h1>
     </v-row>
+    <v-row justify="center">
+        <v-btn v-on:click="logout()">salirte de microsoft XD</v-btn>
+    </v-row>
+</v-container>
 </template>
 
 <script>
@@ -16,6 +21,12 @@
   }),
     
     methods:{
+
+      logout(){
+
+          window.location.href = "https://login.microsoftonline.com/aeaf81cc-0972-471b-8cda-378007902baf/oauth2/logout?post_logout_redirect_uri=http%3A%2F%2Flocalhost:8000%2Flogout"
+      
+      }
 
     }
     ,
